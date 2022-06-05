a2a56312b8be194547db326d2e8ae1284223e1b3
4314215 <101438849+4314215@users.noreply.github.com>
2022-06-05 05:57:24 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -46,7 +46,8 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kiV = [0.0]
     ret.stopAccel = 0.0
 
-    ret.longitudinalActuatorDelayUpperBound = 3.0 # in s default 1.0
+    ret.longitudinalActuatorDelayLowerBound = 0.3
+    ret.longitudinalActuatorDelayUpperBound = 0.3 # in s default 1.0
 
     ret.lateralTuning.pid.kf = 0.00005
     ret.mass = 3982. * CV.LB_TO_KG + STD_CARGO_KG
