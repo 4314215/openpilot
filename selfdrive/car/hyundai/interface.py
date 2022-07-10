3f1385169ab097742385edf1852809b31853d0cc
4314215 <101438849+4314215@users.noreply.github.com>
2022-07-10 10:22:48 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -33,10 +33,10 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.1  # Default delay
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.4
-    tire_stiffness_factor = 1.
+    tire_stiffness_factor = 1.0
 
     ret.stoppingControl = True
-    ret.vEgoStopping = 0.8 # default 1.0
+    ret.vEgoStopping = 1.0
 
     ret.longitudinalTuning.kpV = [0.0]
     ret.longitudinalTuning.kiV = [0.0]
